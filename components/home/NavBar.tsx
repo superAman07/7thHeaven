0943ecfdf51217713aed18995f3110ff9f6884a7
superAman07 <rivalaman123@gmail.com>
2026-01-30 02:45:52 +0530

    added direct orders nav in navbar

--- components/home/NavBar.tsx
+++ components/home/NavBar.tsx
@@ -399,6 +399,7 @@ export default function NavBar() {
                                             <ul className="ht-dropdown right">
                                                 <li><a href="/my-account">My Account</a></li>
                                                 <li><a href="/wishlist">My Wish List</a></li>
+                                                {user && <li><a href="/my-account?tab=orders">My Orders</a></li>}
                                                 {user && <li><a href="/my-account?tab=notifications">Notifications</a></li>}
                                                 {user ? (
                                                     <li><a href="/" onClick={handleLogout}>Logout</a></li>
